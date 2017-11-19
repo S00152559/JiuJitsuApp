9d19a8c9d7bc565d71f6d5604e57b1b1f799e93e
S00152559 <s00152559@mail.itsligo.ie>
2017-11-19 18:35:06 +0000

    app setup

---
+++ JiuJitsuNotes/JiuJitsuNotes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JiuJitsuNotes.MvcApplication" Language="C#" %>
---
+++ JiuJitsuNotes/JiuJitsuNotes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JiuJitsuNotes.Startup))]
+namespace JiuJitsuNotes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
